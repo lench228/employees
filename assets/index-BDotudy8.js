7b0a733403b4ba5ced93bb3478a371d106934684
Lench228 <leonidgneusev@gmail.com>
2025-02-17 15:07:48 +0500

    Updates

--- assets/index-BDotudy8.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-DcX8PbF4.js";const s=()=>e.jsx("div",{className:"text-blue",children:"Главная"});export{s as default};
---
+++ assets/index-CRK9DurQ.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-BmliiyZL.js";const s=()=>e.jsx("div",{className:"text-blue",children:"Главная"});export{s as default};
